aee607676bd35e42f800037734c496509d9b3a0f
Richard Dowd <richarddowd@Richards-MacBook-Pro.local>
2021-11-02 13:22:44 -0700

    finished dog years

--- index.js
+++ index.js
@@ -92,10 +92,12 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+  
+    return age * 7;
 }
 
+console.log(dogYears(7))
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
